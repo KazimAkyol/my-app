743d49d9fdd4218eb38f655cad8dae155084c8b1
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-08 00:54:41 +0100

    sixth commit

--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Quiz.css";
 import { useParams } from "react-router-dom";
+import * as api from "../../api/api";
 
 const Quiz = () => {
   const { difficulty, amount } = useParams();
+
+  const [questionsData, setQuestionsData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await api.fetchQuizData(difficulty, amount);
+      setQuestionsData(data);
+    };
+    getData();
+  }, []);
+
+  console.log(questionsData);
+
   return (
     <div className="quiz">
       {difficulty} - {amount}
