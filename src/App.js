1bed3cc2faf6ef5ea83f1a53956e641755f1a335
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-07 23:58:11 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AIntroduce from "./pages/introduce/AIntroduce";
-
+import Quiz from "./pages/quiz/Quiz";
 
 function App() {
   return (
     <div className="">
       <Router>
         <Routes>
-        <Route path="/" element={<AIntroduce />} />
+          <Route path="/" element={<AIntroduce />} />
+          <Route path="/quiz/:difficulty/:amount" element={<Quiz />} />
         </Routes>
       </Router>
     </div>
--- src/pages/introduce/AIntroduce.jsx
+++ src/pages/introduce/AIntroduce.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./AIntroduce.css"
+import Dropdown from '../../components/dropdown/Dropdown'
 
 const AIntroduce = () => {
+
+    const difficulty = ["easy", "medium", "hard"]
   return (
-    <div className='aintroduce'>AIntroduce</div>
+    <div className='introduce'>
+        <div className='introduce-container'>
+            <img src="https://img.freepik.com/vektoren-premium/quiz-logo-mit-sprechblasen-symbol_149152-813.jpg" alt="" />
+            <Dropdown/>
+        </div>
+    </div>
   )
 }
 
