4336e13e12ff84d2d2d1def6547952e2ab6876ef
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-07 23:50:03 +0100

    second commit

---
+++ src/pages/introduce/AIntroduce.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./AIntroduce.css"
+
+const AIntroduce = () => {
+  return (
+    <div className='aintroduce'>AIntroduce</div>
+  )
+}
+
+export default AIntroduce
